a63029869e4f46113352a6a36c27bfbb940243e2
Marina Ghattas <95654378+marinasamirghattas@users.noreply.github.com>
2022-12-30 20:17:27 +0200

    .

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'https://udagram-api-dev.eba-pb2bev2e.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udagram-api-dev.eba-pb2bev2e.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
