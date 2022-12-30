b6fa05c57e8528ee1638a86887103b4fff5fa8fb
Marina Ghattas <95654378+marinasamirghattas@users.noreply.github.com>
2022-12-30 21:03:32 +0200

    .

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   appName: 'Udagram',
   apiHost: 'http://udagram-api-dev.eba-pb2bev2e.us-east-1.elasticbeanstalk.com/api/v0'
 };
